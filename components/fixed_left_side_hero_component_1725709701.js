f7d60c16ac50f3609d8718b19711ea62db9315b1
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-09-07 11:48:21 +0000

    Replace placeholders across project

--- components/fixed_left_side_hero_component_1725709701.js
+++ components/fixed_left_side_hero_component_1725709701.js
@@ -17,9 +17,9 @@ INSTRUCTION: - The grey hero sub text below it
                 <div class="min-h-full w-full max-w-xl mx-auto flex flex-col justify-start px-4 sm:px-6 pt-36 pb-20 lg:py-20 flex-1" id="quote-header">
                     <div class="grow flex flex-col justify-center" id="quote-content">
                         <div class="space-y-3" id="quote-container">
-                            <div id="quote-title" class="font-caveat text-3xl text-blue-500">HERO_TEXT_BEFORE_TITLE</div>
-                            <h1 id="company-name" class="h1 font-orbiter font-bold text-white">HERO_TITLE</h1>
-                            <time id="quote-date" class="block font-caveat text-xl text-slate-400">HERO_SUBTITLE</time>
+                            <div id="quote-title" class="font-caveat text-3xl text-blue-500">Introducing</div>
+                            <h1 id="company-name" class="h1 font-orbiter font-bold text-white">The Ultimate Blank Canvas</h1>
+                            <time id="quote-date" class="block font-caveat text-xl text-slate-400">Unlimited possibilities, endless creativity</time>
                         </div>
                     </div>
                 </div>
